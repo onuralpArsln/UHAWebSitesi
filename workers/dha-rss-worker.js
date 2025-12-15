8a8dd1270f96e9d1301ea255ea22041750cc6beb
onuralpArlsn <onuralparslan97@gmail.com>
2025-12-15 14:56:11 +0300

    ban update

--- workers/dha-rss-worker.js
+++ workers/dha-rss-worker.js
@@ -26,7 +26,7 @@ const SETTINGS = {
   maxImages: 2,
   maxVideos: 1,
   fallbackCategory: 'Gündem',
-  bannedTopics: ["Yurt Haber"],
+  bannedTopics: ["Yurt Haber", "Ek fotoğraflar"],
   defaultLimit: 20,
   defaultFeedUrl: 'https://dhaabone.dha.com.tr/rss/1719/k9quL7DqdugGLn4kKrTMzmHbRWQN5JQZ4wfCwMuJiOE64o3-B7R_qu33sYG8kMYZHDqtewhItlDOPuc=',
   defaultLogFile: null,
